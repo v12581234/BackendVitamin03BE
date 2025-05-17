b0af174bbe906666882d08946aea91485f1e7954
v12581234 <vinhnguyen125812@gmail.com>
2025-05-17 16:22:24 +0700

    Commit01: Setup

---
+++ src/Main/Java/Main.java
@@ -0,0 +1,8 @@
+package Main.Java;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Vitamin Codee 03 - Git! - AnhDev");
+        System.out.println(">> Hello admin");
+    }
+}
